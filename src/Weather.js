ac87a91ca3f3dcca3e7ce5df38360cdeca89b27a
Emilija Aceska <69395075+EmilijaAceska@users.noreply.github.com>
2021-02-17 14:59:41 +0100

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -3,25 +3,25 @@ import './Weather.css';
 
 export default function Weather(){
   return (
-     <div className="weather-container">
+     <div className="Weather">
           <h1>Prilep, North Macedonia Weather</h1>
-          <p>Wednesday 12:55</p>     
+          <p className="currentDate">Wednesday 12:55</p>     
           
           <div className="row">
             <div className="col-6">
-              <p>
+              <p className="degrees">
               <span>-1</span>
               <span>°C</span>
               </p>
-              <ul>
-                <li>Partly Cloudy</li>
-                <li>Humidity: 5%</li>
-                <li>Wind:1 km/h</li>
+              <ul className="about-weather">
+                <li className="description">Partly Cloudy</li>
+                <li className="humidity">Humidity: 5%</li>
+                <li className="wind-speed">Wind:1 km/h</li>
               </ul>
             </div>
-            <div className="col-6">
-              <i class="fas fa-cloud-sun"></i>
-              <p>
+            <div className="col-6 min-max">
+              <i className="fas fa-cloud-sun"></i>
+              <p className="min-max-deg">
                 <span>1° / </span>
                 <span>-9°</span>
               </p>
