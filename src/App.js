58532fe7cd3e0665ef324793ccbbf0707c06eebe
Emilija Aceska <69395075+EmilijaAceska@users.noreply.github.com>
2021-02-17 13:25:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather React Application</h1>
+      </header>
+      <footer>
+        <p>
+          <a>Open-source code,</a> {" "}
+          by Emilija Aceska
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+
